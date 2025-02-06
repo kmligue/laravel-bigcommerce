4040088b696ba585cf9d540c2964d018fc23843e
Kelvin Martin Ligue <liguekelvin@gmail.com>
2025-02-06 14:06:04 +0800

    improvements

--- src/config/webhooks.php
+++ src/config/webhooks.php
@@ -3,7 +3,7 @@
 return [
     // [
     //     'scope' => 'store/cart/couponApplied',
-    //     'destination' => url('api/'. $store->store_hash .'/store/cart/coupon-applied'),
+    //     'destination' => config('app.url') . '/api/{storeHash}/store/cart/coupon-applied',
     //     'is_active' => true,
     //     'events_history_enabled' => true
     // ]
