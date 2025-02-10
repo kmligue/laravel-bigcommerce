13b671d51f6f411ea0205381e7cf9f32ae69b36e
Kelvin Martin Ligue <liguekelvin@gmail.com>
2025-02-10 20:48:07 +0800

    fix api middleware

--- src/routes/api.php
+++ src/routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth.bc'])->group(function() {
+Route::middleware(['bigcommerce.store.auth'])->group(function() {
     Route::post('api/stores/{storeHash}/billing/{plan}', [\Limonlabs\Bigcommerce\Controllers\BillingController::class, 'store']);
     Route::post('api/stores/{storeHash}/billing/{plan}/select', [\Limonlabs\Bigcommerce\Controllers\BillingController::class, 'select']);
 });
