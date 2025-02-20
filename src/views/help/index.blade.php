d75ffccffd5e6550a53b484a884a6445f6cd360c
Kelvin Martin Ligue <liguekelvin@gmail.com>
2025-02-20 15:36:56 +0800

    BC-633: add redirect url to help form in help page

--- src/views/help/index.blade.php
+++ src/views/help/index.blade.php
@@ -6,6 +6,13 @@
     <div class="bg-white shadow-md p-5 mt-8">
         @include('limonlabs/bigcommerce::layouts.flash')
 
+        @if (request()->query('success') && request()->query('success') == 1)
+            <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative" role="alert">
+                <strong class="font-bold">Success!</strong>
+                <span class="block sm:inline">Your message has been sent successfully.</span>
+            </div>
+        @endif
+
         <form action="https://api.staticforms.xyz/submit" method="post">
             <div class="mb-5">
                 <label for="name" class="block text-sm">Your Name</label>
@@ -25,6 +32,7 @@
             <input type="text" name="honeypot" style="display:none">
             <input type="hidden" name="accessKey" value="{{ env('STATICFORMS_ACCESS_KEY') }}">
             <input type="hidden" name="subject" value="Help from - {{ env('APP_NAME') }}" />
+            <input type="hidden" name="redirectTo" value="{{ url('/' . $storeHash . '/help?success=1') }}">
 
             <div class="mb-5 text-right">
                 <input type="submit" class="bg-[#4b71fc] text-white px-3 py-1 rounded" value="Submit" />
