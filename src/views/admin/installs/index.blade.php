32e6fbc2d376877cde7eda461c9592c04a769f7f
Kelvin Martin Ligue <liguekelvin@gmail.com>
2025-03-24 10:01:33 +0800

    display email if store name is empty

--- src/views/admin/installs/index.blade.php
+++ src/views/admin/installs/index.blade.php
@@ -122,7 +122,14 @@
                                 <a href="mailto:{{ $store->user_email }}">
                                     <svg class="w-3 h-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48L48 64zM0 176L0 384c0 35.3 28.7 64 64 64l384 0c35.3 0 64-28.7 64-64l0-208L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z"/></svg>
                                 </a> 
-                                <span>{{ $store->first_name }} {{ $store->last_name }}</span>
+                                @php
+                                    $name = $store->first_name . ' ' . $store->last_name;
+
+                                    if (empty(trim($name))) {
+                                        $name = $store->user_email;
+                                    }
+                                @endphp
+                                <span>{{ $name }}</span>
                             </div>
                         </td>
                         <td class="border-b border-[#d1d5db] p-4 pl-8 text-slate-500">
