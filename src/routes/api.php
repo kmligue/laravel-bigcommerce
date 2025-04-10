b1411cc57aba514baf5f186b8f19e69a7b3124c2
Kelvin Martin Ligue <liguekelvin@gmail.com>
2025-04-10 18:01:24 +0800

    BC-764: Reorder billing route definitions for better clarity and maintainability in the API routes.

--- src/routes/api.php
+++ src/routes/api.php
@@ -3,8 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['bigcommerce.store.auth'])->group(function() {
-    Route::post('api/stores/{storeHash}/billing/{plan}', [\Limonlabs\Bigcommerce\Controllers\BillingController::class, 'store']);
-    Route::post('api/stores/{storeHash}/billing/{plan}/select', [\Limonlabs\Bigcommerce\Controllers\BillingController::class, 'select']);
-    Route::post('api/stores/{storeHash}/billing/trial/change', [\Limonlabs\Bigcommerce\Controllers\BillingController::class, 'trialChange']);
     Route::post('api/stores/{storeHash}/billing/cancel', [\Limonlabs\Bigcommerce\Controllers\BillingController::class, 'cancel']);
+    Route::post('api/stores/{storeHash}/billing/trial/change', [\Limonlabs\Bigcommerce\Controllers\BillingController::class, 'trialChange']);
+    Route::post('api/stores/{storeHash}/billing/{plan}/select', [\Limonlabs\Bigcommerce\Controllers\BillingController::class, 'select']);
+    Route::post('api/stores/{storeHash}/billing/{plan}', [\Limonlabs\Bigcommerce\Controllers\BillingController::class, 'store']);
 });
